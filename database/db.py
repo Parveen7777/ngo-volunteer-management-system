1c29b5aa4b116407d77e549a9b36273096fbe3ab
Parveen7777 <192462005.simats@saveetha.com>
2026-08-15 15:21:29 +0530

    Create db.py

---
+++ database/db.py
@@ -0,0 +1,14 @@
+import sqlite3
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "ngo.db")
+
+def get_connection():
+    conn = sqlite3.connect(DB_PATH)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+def close_connection(conn):
+    if conn:
+        conn.close()
