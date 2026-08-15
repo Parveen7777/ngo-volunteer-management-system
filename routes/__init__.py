9c77d5ba81f3c4accf618ac23aca3257cf7811e5
Parveen7777 <192462005.simats@saveetha.com>
2026-08-15 16:00:16 +0530

    Initialize routes by importing route blueprints

---
+++ routes/__init__.py
@@ -0,0 +1,6 @@
+from .auth_routes import auth_bp
+from .dashboard_routes import dashboard_bp
+from .volunteer_routes import volunteer_bp
+from .event_routes import event_bp
+from .attendance_routes import attendance_bp
+from .report_routes import report_bp
