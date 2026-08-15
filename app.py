8ef0e243961b1a728590d0065b86e761413ed1f1
Parveen7777 <192462005.simats@saveetha.com>
2026-08-15 14:33:50 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from database.db import init_db
+
+app = Flask(__name__)
+app.config.from_object('config.Config')
+
+# Initialize database
+init_db()
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
